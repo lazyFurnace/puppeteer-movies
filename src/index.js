46e4a568d6505d32501ff2f747a523f7ff140958
qi.li <787139384@qq.com>
2018-10-24 03:35:59 +0800

    feat: 对于该网站后端渲染数据所以不需要各种事件

--- src/index.js
+++ src/index.js
@@ -9,36 +9,30 @@ const puppeteer = require('puppeteer');
     const indexPage = await browser.newPage();
     const moviesData = [];
 
-    indexPage.on('domcontentloaded', async () => {
-        const detailsUrl = await indexPage.$eval('.list-page', node => {
-            let detailsHref = node.querySelectorAll('ul>li>p:first-child>a');
-            detailsHref = Array.from(detailsHref).map(item => item.href);
-            return detailsHref;
-        });
-
-        const detaiPage = await browser.newPage();
-
-        for (let i = 0; i < detailsUrl.length; i++) {
-            await detaiPage.goto(detailsUrl[i]);
-            await detaiPage.waitFor(2000);
-            const moviesTitle = await detaiPage.$eval('.movie', node => {
-                return node.querySelector('h1').textContent;
-            })
-            const moviesIntroduce = await detaiPage.$eval('.inst', node => {
-                return node.querySelector('.content2').childNodes[3].textContent;
-            })
-            moviesData.push({
-                title: moviesTitle,
-                introduce: moviesIntroduce
-            })
-            
-        }
-        await detaiPage.close();
-        console.log(moviesData);
-    });
-
     await indexPage.goto('http://www.ckck.tv/kh/Index.html');
 
+    const detailsUrl = await indexPage.$eval('.list-page', node => {
+        let detailsHref = node.querySelectorAll('ul>li>p:first-child>a');
+        detailsHref = Array.from(detailsHref).map(item => item.href);
+        return detailsHref;
+    });
 
+    const detaiPage = await browser.newPage();
 
+    for (let i = 0; i < detailsUrl.length; i++) {
+        await detaiPage.goto(detailsUrl[i]);
+        const moviesTitle = await detaiPage.$eval('.movie', node => {
+            return node.querySelector('h1').textContent;
+        })
+        const moviesIntroduce = await detaiPage.$eval('.inst', node => {
+            return node.querySelector('.content2').childNodes[3].textContent;
+        })
+        moviesData.push({
+            title: moviesTitle,
+            introduce: moviesIntroduce
+        })
+    }
+
+    await detaiPage.close();
+    console.log(moviesData);
 })()
