e7857ed08691c54e08736f3c042276cdf0cb7230
qi.li <787139384@qq.com>
2018-10-24 03:30:47 +0800

    feat: 完成第一页详情抓取

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const puppeteer = require('puppeteer');
         headless: false
     })
     const indexPage = await browser.newPage();
+    const moviesData = [];
 
     indexPage.on('domcontentloaded', async () => {
         const detailsUrl = await indexPage.$eval('.list-page', node => {
@@ -15,12 +16,25 @@ const puppeteer = require('puppeteer');
             return detailsHref;
         });
 
-        detailsUrl.forEach(async item => {
-            const detaiPage = await browser.newPage();
-            await detaiPage.goto(item);
+        const detaiPage = await browser.newPage();
 
-        });
-
+        for (let i = 0; i < detailsUrl.length; i++) {
+            await detaiPage.goto(detailsUrl[i]);
+            await detaiPage.waitFor(2000);
+            const moviesTitle = await detaiPage.$eval('.movie', node => {
+                return node.querySelector('h1').textContent;
+            })
+            const moviesIntroduce = await detaiPage.$eval('.inst', node => {
+                return node.querySelector('.content2').childNodes[3].textContent;
+            })
+            moviesData.push({
+                title: moviesTitle,
+                introduce: moviesIntroduce
+            })
+            
+        }
+        await detaiPage.close();
+        console.log(moviesData);
     });
 
     await indexPage.goto('http://www.ckck.tv/kh/Index.html');
